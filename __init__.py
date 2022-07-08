38a4f17207073a8c69a8bb59bd510213d5979c65
pathmapper <pathmapper@posteo.de>
2022-07-08 06:27:52 +0200

    Add files

---
+++ __init__.py
@@ -0,0 +1,24 @@
+"""
+***************************************************************************
+XPlan-Reader
+QGIS plugin
+
+        Date                 : July 2022
+        Copyright            : (C) 2022 by Kreis Viersen
+        Email                : open@kreis-viersen.de
+
+***************************************************************************
+
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+def classFactory(iface):
+    from .xplan_reader import XplanReader
+    return XplanReader(iface)
